d305a519bb2d95ce1b13f63c340882653c211bb4
Dave Trost <davetrost@gmail.com>
2019-08-30 15:15:27 -0700

    server setup (it's alive)

---
+++ server.js
@@ -0,0 +1,53 @@
+// Load Environment Variables from the .env file
+require('dotenv').config();
+
+// Application Dependencies
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const client = require('./lib/client');
+
+// Auth
+const ensureAuth = require('./lib/auth/ensure-auth');
+const createAuthRoutes = require('./lib/auth/create-auth-routes');
+const authRoutes = createAuthRoutes({
+    selectUser(email) {
+        return client.query(`
+        SELECT id, email, hash, display_name as "name" 
+        FROM users
+        WHERE email = $1;
+        `,
+        [email]
+        ).then(result => result.rows[0]);
+    },
+    insertUser(user, hash) {
+        return client.query(`
+            INSERT into users (email, hash, display_name)
+            VALUES ($1, $2, $3)
+            RETURNING id, email, display_name as "name";
+        `,
+        [user.email, hash, user.name]
+        ).then(result => result.rows[0]);
+    }
+});
+
+// Application Setup
+const app = express();
+const PORT = process.env.PORT;
+app.use(morgan('dev')); // http logging
+app.use(cors()); // enable CORS request
+app.use(express.static('public')); // enable serving files from public
+app.use(express.json()); // enable reading incoming json data
+
+// setup authentication routes
+app.use('/api/auth', authRoutes);
+
+// everything that starts with "/api" below here requires an auth token!
+//app.use('/api', ensureAuth);
+
+
+
+// Start the server
+app.listen(PORT, () => {
+    console.log('server running on PORT', PORT);
+});
