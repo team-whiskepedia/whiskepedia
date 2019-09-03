58ccde2e874e672585add7836752e00c72facae7
sanvean74 <dlambertpdx@gmail.com>
2019-09-03 15:37:28 -0700

    bringing in latest changes

---
+++ server.js
@@ -0,0 +1,96 @@
+// Load Environment Variables from the .env file
+require('dotenv').config();
+
+// Application Dependencies
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const client = require('./lib/client');
+
+// Auth
+const ensureAuth = require('./lib/auth/ensure-auth');
+const createAuthRoutes = require('./lib/auth/create-auth-routes');
+const authRoutes = createAuthRoutes({
+    selectUser(email) {
+        return client.query(`
+        SELECT id, email, hash, display_name as "name" 
+        FROM users
+        WHERE email = $1;
+        `,
+        [email]
+        ).then(result => result.rows[0]);
+    },
+    insertUser(user, hash) {
+        console.log(user);
+        return client.query(`
+            INSERT into users (email, hash, display_name)
+            VALUES ($1, $2, $3)
+            RETURNING id, email, display_name as "name";
+        `,
+        [user.email, hash, user.displayName]
+        ).then(result => result.rows[0]);
+    }
+});
+
+// Application Setup
+const app = express();
+const PORT = process.env.PORT;
+app.use(morgan('dev')); // http logging
+app.use(cors()); // enable CORS request
+app.use(express.static('public')); // enable serving files from public
+app.use(express.json()); // enable reading incoming json data
+
+// setup authentication routes
+app.use('/api/auth', authRoutes);
+
+// everything that starts with "/api" below here requires an auth token!
+app.use('/api', ensureAuth);
+
+app.get('/api/whiskeys', (req, res) => {
+    client.query(`
+        SELECT
+            id,
+            title,
+            list_img_url,
+            detail_img_url,
+            region,
+            rating,
+            price,
+            flavor_1,
+            flavor_2,
+            flavor_3,
+            description
+        FROM whiskeys;
+    `)
+        .then(result => {
+            res.json(result.rows);
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err.message || err
+            });
+        });
+});
+
+app.get('/api/flavors', (req, res) => {
+    client.query(`
+        SELECT
+            name,
+            category,
+            broad_category AS "broadCategory"
+        FROM flavors;
+    `)
+        .then(result => {
+            res.json(result.rows);
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err.message || err
+            });
+        });
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log('server running on PORT', PORT);
+});
