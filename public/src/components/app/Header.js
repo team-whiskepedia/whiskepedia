3649cf871c4c0f6a8733a554dd2e5aacf6dd2e77
JOHN NELSON <JohnNelson4850@gmail.com>
2019-09-03 15:10:40 -0700

    hides MyBottles, logs in and out properly

--- public/src/components/app/Header.js
+++ public/src/components/app/Header.js
@@ -9,21 +9,44 @@ export class Header extends Component {
         hamburgerMenu.addEventListener('click', () => {
             mobileNavLinksList.classList.toggle('displayed');
         });
+        const loginButton = dom.querySelector('.log-in');
+        const logoutButton = dom.querySelector('.log-out');
+        const loginButtonMobile = dom.querySelector('.log-in-mobile');
+        const logoutButtonMobile = dom.querySelector('.log-out-mobile');
+        const myBottles = dom.querySelector('.bottles');
+        const myBottlesMobile = dom.querySelector('.bottles-mobile');
+        console.log(myBottles, myBottlesMobile);
+        
         if(store.hasToken()) {
-            const button = dom.querySelector('.log-out');
-            button.remove('hidden');
+            logoutButton.classList.remove('no-display');
+            logoutButtonMobile.classList.remove('no-display');
+            loginButton.classList.add('no-display');
+            loginButtonMobile.classList.add('no-display');
+            myBottles.classList.remove('no-display');
+            myBottlesMobile.classList.remove('no-display');
 
-            button.addEventListener('click', () => {
-                store.removeToken();
-                location = './';
-            });
         }
-
-
+        if(!store.hasToken() && location.pathname !== '/auth.html') {
+            loginButton.classList.remove('no-display');
+            loginButtonMobile.classList.remove('no-display');
+            logoutButton.classList.add('no-display');
+            logoutButtonMobile.classList.add('no-display');
+            myBottles.classList.add('no-display');
+            myBottlesMobile.classList.add('no-display');
+            location = './auth.html';
+        } 
+        logoutButton.addEventListener('click', () => {
+            store.removeToken();
+            store.removeUser();
+            loginButton.classList.remove('.no-display');
+            logoutButton.classList.add('.no-display');
+            location = './';
+        });
     }
+    
 
     renderHTML() {
-        const user = store.getUser() || 'Guest';
+        const user = store.getUser() || 'Not Logged In';
 
         return /*html*/`
             <header class="header dark">
@@ -31,9 +54,9 @@ export class Header extends Component {
                     <a><i class="material-icons">menu</i></a>
                     <ul class='mobile-nav-links'>
                         <li><a href="#">Browse</a></li>
-                        <li><a href="#">My Bottles</a></li>
-                        <li><a href="/auth.html">Login</a></li>
-                        <li><a class ="log-out" href="#">Logout</a></li>
+                        <li class="bottles-mobile no-display"><a href="#">My Bottles</a></li>
+                        <li class="log-in-mobile"><a href="/auth.html">Login</a></li>
+                        <li class="log-out-mobile no-display"><a href="/auth.html">Logout</a></li>
                     </ul>
                 </nav>
                 <div class="logo-container">
@@ -42,13 +65,13 @@ export class Header extends Component {
                 </div>
                 <nav class="desktop-nav hide-sm">
                     <div>
-                        <a href="#">Logged in as ${user}</a>
+                        <a href="#">${user}</a>
                     </div>
                     <ul class="flex">
                         <li><a href="#">Browse</a></li>
-                        <li><a href="#">My Bottles</a></li>
-                        <li><a href="#">Login</a></li>
-                        <li><a href="#">Logout</a></li>
+                        <li class="bottles no-display"><a href="#">My Bottles</a></li>
+                        <li  class="log-in"><a class="log-in" href="/auth.html">Login</a></li>
+                        <li class="log-out no-display"><a href="/auth.html">Logout</a></li>
                     </ul>
                 </nav>
             </header>
