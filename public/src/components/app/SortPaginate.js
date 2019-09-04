da61fc5efbc2373c5bca02a8be74a2dbde3d86ec
sanvean74 <dlambertpdx@gmail.com>
2019-09-04 15:59:38 -0700

    pagination

--- public/src/components/app/SortPaginate.js
+++ public/src/components/app/SortPaginate.js
@@ -1,43 +1,13 @@
 import Component from '../Component.js';
+import hashStorage from '../../services/hash-storage.js';
 
 export class SortPaginate extends Component {
 
     onRender(dom) {
         
-
-
-        // const flavorsByBroadCategory = this.props.flavors.reduce((broadCategories, flavor) => {
-        //     const existingIndex = broadCategories.findIndex((item) => item.category === flavor.broadCategory);
-        //     if(existingIndex >= 0) {
-        //         broadCategories[existingIndex].flavors.push(flavor.name);
-        //     } 
-        //     else if(flavor.broadCategory !== 'IGNORE') {
-        //         broadCategories.push({ category: flavor.broadCategory, flavors: [flavor.name] });
-        //     }
-        //     return broadCategories;
-        // }, []);
-        // flavorsByBroadCategory.sort(objA => objA.category.toLowerCase() === 'tastes' ? -1 : 1);
-        
-        // const searchPane = dom.querySelector('#search-pane');
-        // flavorsByBroadCategory.forEach(category => {
-        //     const searchCategory = new SearchCategory({ 
-        //         category: category,
-        //         //onUpdate: this.props.onUpdate,
-        //         //onRemove: this.props.onRemove,
-        //     }); 
-        //     searchPane.appendChild(searchCategory.renderDOM());
-        // });
     }
 
     renderHTML() {
-        const currentPage = this.props.pages.current;
-        const lastPage = this.props.pages.last;
-        const pagePrevPrev = (currentPage <= 2) ? '' : /*html*/`<a href="#" class="page previous-previous">${currentPage - 2}</a>`;
-        const pagePrev = (currentPage <= 1) ? '' : /*html*/`<a href="#" class="page previous">${currentPage - 1}</a>`;
-        const pageCurrent = /*html*/`<span class="page active">${currentPage}</span>`;
-        const pageNext = (lastPage - currentPage <= 1) ? '' : /*html*/`<a href="#" class="page next">${currentPage + 1}</a>`;
-        const pageNextNext = (lastPage - currentPage <= 2) ? '' : /*html*/`<a href="#" class="page next-next">${currentPage + 2}</a>`;
-
         return /*html*/`
             <section id="sort-pane">
                 <div class="sorting">
@@ -51,15 +21,6 @@ export class SortPaginate extends Component {
                         <option value="price-low-high">Price: Low-High</option>
                         <option value="price-high-low">Price: High-Low</option>
                     </select>
-                </div>
-                <div class="pagination">
-                    <a href="#" class="page first">first</a>
-                    ${pagePrevPrev}
-                    ${pagePrev}
-                    ${pageCurrent}
-                    ${pageNext}
-                    ${pageNextNext}
-                    <a href="#" class="page last">last</a>
                 </div>
             </section>
         `;
