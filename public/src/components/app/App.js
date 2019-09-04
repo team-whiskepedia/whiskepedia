9dfd41c2d9e46000446a4fe8fa0677cd16192be0
Dave Trost <davetrost@gmail.com>
2019-09-03 17:59:40 -0700

    splash component and styling

--- public/src/components/app/App.js
+++ public/src/components/app/App.js
@@ -15,7 +15,31 @@ export class App extends Component {
         return /*html*/`
             <div>
                 <!-- header goes here -->
-                <main></main>
+                <main class="container">
+                    <h3>Welcome!</h3>
+                    <div class="slideshow-container">
+                        <img src="assets/whiskey-sprite.png" alt="bottles of whiskey">
+                    </div>
+                    <section class="info">
+                        <p>
+                            Whiskepedia contains hundreds of bottles of whiskey, in various styles, 
+                            cataloged by their flavor profiles.
+                            <ul>
+                                <li>Explore new flavor profiles. </li>
+                                <li>Identify the flavors that work best for your palate.</li>
+                                <li>Fine-tune the profile of your next bottle.</li>
+                            </ul>
+                        </p>
+                        <div class="row">
+                            <p>Login now to explore your customized flavor profile!</p>
+                            <a href="/auth.html">Login</a>
+                        </div>
+                        <div class="row">
+                            <p>Or go directly to browse the database</p>
+                            <a href="/browse.html">Browse</a>
+                        </div>
+                    </section>
+                </main>
             </div>
         `;
     }
