c9c8cfdc44bfc21cf3013df5e54d11e3246a18b6
Dave Trost <davetrost@gmail.com>
2019-09-04 09:39:48 -0700

    sort and paginate component

--- public/src/components/whiskeys/WhiskeyList.js
+++ public/src/components/whiskeys/WhiskeyList.js
@@ -1,9 +1,13 @@
 import Component from '../Component.js';
 import WhiskeyItem from './WhiskeyItem.js';
+import { SortPaginate } from '../app/SortPaginate.js';
 
 class WhiskeyList extends Component {
     onRender(dom) {
         const whiskeys = this.props.whiskeys;
+
+        const sortPaginateBar = new SortPaginate();
+        dom.appendChild(sortPaginateBar.renderDOM());
 
         whiskeys.forEach(whiskey => {
             const props = { whiskey: whiskey };
